3bb321b6dde8c4a3875eadc5798850826e350b6d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 15:39:17 +0300

    Open file dialog

--- iPatch/ViewController.swift
+++ iPatch/ViewController.swift
@@ -8,19 +8,65 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var ipaURL:URL?
+    var debURL:URL?
 
+    @IBOutlet weak var ipaFileNameText: NSTextField!
+    @IBOutlet weak var debFileNameText: NSTextField!
+    @IBOutlet weak var displayNameTextField: NSTextField!
+    @IBOutlet weak var injectSubstrateButton: NSButton!
+    @IBOutlet weak var patchButton: NSButton!
+    
+    @IBAction func selectIPA(_ sender: NSButton) {
+        let panel: NSOpenPanel = NSOpenPanel()
+        panel.allowsMultipleSelection = false
+        panel.canChooseDirectories = false
+        panel.canChooseFiles = true
+        panel.isFloatingPanel = false
+        panel.allowedFileTypes = ["ipa"]
+        panel.beginSheetModal(for: self.view.window!, completionHandler: { response in
+            if response == NSApplication.ModalResponse.OK {
+                self.ipaURL = panel.url
+                self.updateUI()
+            }
+        })
+    }
+    
+    @IBAction func selectDeb(_ sender: NSButton) {
+        let panel: NSOpenPanel = NSOpenPanel()
+        panel.allowsMultipleSelection = false
+        panel.canChooseDirectories = false
+        panel.canChooseFiles = true
+        panel.isFloatingPanel = false
+        panel.allowedFileTypes = ["deb"]
+        panel.beginSheetModal(for: self.view.window!, completionHandler: { response in
+            if response == NSApplication.ModalResponse.OK {
+                self.debURL = panel.url
+                self.updateUI()
+            }
+        })
+    }
+    @IBAction func patchButtonClicked(_ sender: NSButton) {
+        // TODO: Some stuff here...
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        patchButton.isEnabled = false
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    func updateUI() {
+        if ipaURL != nil, debURL != nil {
+            patchButton.isEnabled = true
         }
+        ipaFileNameText.stringValue = ipaURL?.lastPathComponent ?? "None selected"
+        debFileNameText.stringValue = debURL?.lastPathComponent ?? "None selected"
+        displayNameTextField.stringValue = (ipaURL?.lastPathComponent ?? "").components(separatedBy: " ").first ?? ""
     }
-
 
 }
 
